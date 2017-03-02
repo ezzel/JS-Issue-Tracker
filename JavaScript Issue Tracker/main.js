566f9914c40f37974bcce9377a9407f2e807daba
Ezzeldien Mohammed <ezzel123@gmail.com>
2017-03-02 17:04:49 -0500

    Update main.js

--- JavaScript Issue Tracker/main.js
+++ JavaScript Issue Tracker/main.js
@@ -1,12 +1,13 @@
+//An event listner for the sumbit button on the form
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
-
+//The save function will fire once you click the add button
 function saveIssue(e) {
     var issueDesc = document.getElementById('issueDescInput').value;
     var issueSeverity = document.getElementById('issueSeverityInput').value;
     var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
     var issueId = chance.guid();
     var issueStatus = 'Open';
-    
+    // issue object instantiated to hold the values of the variables assigned above
     var issue = {
         id: issueId,
         description: issueDesc,
@@ -14,23 +15,24 @@ function saveIssue(e) {
         assignedTo: issueAssignedTo,
         status : issueStatus
     };
-    
+    //Checks the local storage/for the issue file if null create an array then push the new object to the array then set the file to the array in JSON format.
     if (localStorage.getItem('issues') === null) {
         var issues = [];
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
+    //Else get the array of the JSON file if the file isn't null then add the new object to the array then reset the JSON file in a format!
     } else {
     var issues = JSON.parse(localStorage.getItem('issues'));
         issues.push(issue);
         localStorage.setItem('issues',JSON.stringify(issues));
     }
-    
+    //Resets the issue input form.
     document.getElementById('issueInputForm').reset();
-    
+    //Call the fetch function.
     fetchIssues();
     e.preventDefault();
 }
-
+//Function sets the status to a issue that has been closed
 function setStatusClosed(id) {
   var issues = JSON.parse(localStorage.getItem('issues'));
 
@@ -58,7 +60,7 @@ function deleteIssue(id) {
 
   fetchIssues();
 }
-
+//Function that fetches issues from the JSON file.
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
     
